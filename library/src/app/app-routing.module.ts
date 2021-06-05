5ffda94a8f9cc3402fb402ce6eead01027049361
Baba Fakruddin <fakruddinbaba16@gmail.com>
2021-06-05 12:13:54 +0530

    changes in sprint-0

--- library/src/app/app-routing.module.ts
+++ library/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from 'src/components/login/login.component';
+import { SearchComponent } from 'src/components/search/search.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- library/src/app/app.module.ts
+++ library/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from 'src/components/login/login.component';
+import { SearchComponent } from 'src/components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
